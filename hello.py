0f8adab7386385083e7fe1b4be70a6a9c012de9d
lamng8864 <lamtungnguyen.dk2@gmail.com>
2025-11-03 15:45:12 +0700

    made a resume, took about 2h-3h (idk) in total, html and css. Not immaculate but usable feels like forever

--- hello.py
+++ hello.py
@@ -2,3 +2,9 @@ def say_hello(name):
     print("Hello, " + name)
 
 say_hello("VS Code")
+
+
+# i want to make a program that format texts in .csv files to make sql data input faster
+# data types that can include characters include: [TEXT,BOOLEAN,ENUM,JSON]
+# but still, most of them still need the "". Excluding the boolean
+# So we just need to check whether there is character then check if that character is true/false or null or not then put the "" around them.
